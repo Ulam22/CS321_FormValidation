575b4531787f22c7550dd002d429950303c60c91
Katie2319 <wind@simmons.edu>
2021-03-03 12:09:06 -0500

    added password match function

---
+++ form.js
@@ -0,0 +1,12 @@
+var check = function() {
+   if (document.getElementById('pass').value ==
+     document.getElementById('confirmPass').value) {
+      document.getElementById('message').innerHTML = 'Passwords Match';
+      document.getElementById('message').style.color = 'green';
+     return true;
+   } else {
+   document.getElementById('message').innerHTML = 'Passwords NOT Matching';
+   document.getElementById('message').style.color = 'red';
+     return false;
+   }
+ }
