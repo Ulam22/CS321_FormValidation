fac6f93f1733c080b6e1cf3f8141f010a84c5bc0
Ulam22 <67839745+Ulam22@users.noreply.github.com>
2021-03-08 11:46:14 -0500

    more form validation

--- form.js
+++ form.js
@@ -1,4 +1,25 @@
-var check = function() {
+const form = document.getElementById('CreateAccount')
+const firstName = document.getElementById('first')
+const password = document.getElementById('pass')
+const errorMessage = document.getElementById('errorMessage')
+
+
+form.addEventListener('submit', (e) => {
+  let messages = []
+  if (firstName.value === '' || firstName.value == null) {
+    messages.push('Name is required')
+  }
+  if (password.value === 'password') {
+    messages.push('Password cannot be password')
+  }
+  if (password.length <= 7) {
+    messages.push('Password cannot be less than 8 characters')
+  }
+  if (messages.length > 0) {
+    e.preventDefault()
+    errorElement.innerText = messages.join(', ')
+  }
+
    if (document.getElementById('pass').value ==
      document.getElementById('confirmPass').value) {
       document.getElementById('message').innerHTML = 'Passwords Match';
@@ -10,3 +31,5 @@ var check = function() {
      return false;
    }
  }
+
+)
