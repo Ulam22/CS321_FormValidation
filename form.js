a2b526f5c492d7e7c5f6262c8e6fa72b7c671121
Katie2319 <wind@simmons.edu>
2021-03-08 15:58:47 -0500

    added field check function

--- form.js
+++ form.js
@@ -33,3 +33,57 @@ function passwordMatch() {
      return false;
    }
 }
+function check(){
+  if (document.getElementById('pass').value ==
+  document.getElementById('confirmPass').value) {
+   document.getElementById('message').innerHTML = 'Passwords Match';
+   document.getElementById('message').style.color = 'green';
+  return true;
+  } else {
+  document.getElementById('message').innerHTML = 'Passwords NOT Matching';
+  document.getElementById('message').style.color = 'red';
+  return false;
+  }
+  }
+  
+  function checkFields() { 
+    if (document.getElementById('first').value == "")  { 
+        window.alert("Please enter your first name."); 
+        return false; 
+    } 
+  
+    if (document.getElementById('last').value == "")  { 
+        window.alert("Please enter your last name.");  
+        return false; 
+    } 
+    if (document.getElementById('phone').value == "") { 
+      window.alert( 
+        "Please enter your phone number"); 
+      return false; 
+  } 
+  
+    if (document.getElementById('address').value == "") { 
+        window.alert( 
+          "Please enter your home address"); 
+        return false; 
+    } 
+  
+    if (document.getElementById('email').value == "") { 
+        window.alert( 
+          "Please enter your email."); 
+        return false; 
+    } 
+  
+    if (document.getElementById('pass').value == "") { 
+        window.alert("Please enter your password");  
+        return false; 
+    } 
+  
+    if (document.getElementById('confirmPass').value == "") { 
+      window.alert("Please enter your password");  
+      return false; 
+  }
+    window.alert("From successfully submitted"); 
+    return true;
+     
+  } 
