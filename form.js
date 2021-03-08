12b7b41ed000a4c5b6b20679678b1b253e1849f2
Katie2319 <wind@simmons.edu>
2021-03-08 15:43:42 -0500

    fixed password function

--- form.js
+++ form.js
@@ -22,16 +22,20 @@ form.addEventListener('submit', (e) => {
  //^ might just delete all this
 
 
-   if (document.getElementById('pass').value ==
-     document.getElementById('confirmPass').value) {
-      document.getElementById('message').innerHTML = 'Passwords Match';
-      document.getElementById('message').style.color = 'green';
-     return true;
-   } else {
-   document.getElementById('message').innerHTML = 'Passwords NOT Matching';
-   document.getElementById('message').style.color = 'red';
-     return false;
-   }
+  
  }
 )}
+}
+
+function check(){
+if (document.getElementById('pass').value ==
+document.getElementById('confirmPass').value) {
+ document.getElementById('message').innerHTML = 'Passwords Match';
+ document.getElementById('message').style.color = 'green';
+return true;
+} else {
+document.getElementById('message').innerHTML = 'Passwords NOT Matching';
+document.getElementById('message').style.color = 'red';
+return false;
+}
 }
